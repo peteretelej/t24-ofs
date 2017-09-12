1ab76c315c5305e5b4f2ebdf89d626610ed11499
Peter Etelej <peter@etelej.com>
2017-09-12 08:13:42 +0300

    Offline First

---
+++ sw.js
@@ -0,0 +1,39 @@
+var cacheName="ofs-v1"
+
+self.addEventListener("install", function(event){
+	event.waitUntil(
+		caches.open(cacheName).then(function(cache){
+			cache.addAll([
+				"/",
+				"/favicon.ico",
+				"/sw.js",
+				"/manifest.json",
+				"/css/foundation.min.css",
+				"/js/vue.min.js",
+				"/js/clipboard.min.js",
+				"/js/main.js"
+			])
+		})
+	)
+})
+self.addEventListener("activate", function(event){
+	event.waitUntil(
+		caches.keys().then(function(keys){
+			keys.forEach(function(key){
+				if (key!== cacheName){
+					caches.delete(key)
+				}
+			})
+		})
+	)
+})
+
+self.addEventListener("fetch", function(event){
+	event.respondWith(
+		caches.open(cacheName).then(function(cache){
+			return cache.match(event.request).then(function(response){
+				return response ||fetch(event.request)
+			})
+		})
+	)
+})
